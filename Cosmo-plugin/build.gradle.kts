9a74874c6a8be538d01caeecb76c3daa4afa8cf0
SenseiJu <chathataranjit@yahoo.co.uk>
2021-05-17 15:48:29 +0100

    Updated to version 0.3.0

--- Cosmo-plugin/build.gradle.kts
+++ Cosmo-plugin/build.gradle.kts
@@ -1,6 +1,6 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val version = "0.2.0"
+val version = "0.3.0"
 
 repositories {
     maven("https://papermc.io/repo/repository/maven-public/")
